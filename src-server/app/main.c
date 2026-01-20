78f066a01577de9be6c1e1108a631eb646b1581f
Yyax13 <Yyax13@proton.me>
2026-01-20 15:26:12 -0300

    feat(project): Created basic architecture structure with empty files

---
+++ src-server/app/main.c
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2026-Present ScorpionC2 public-person "Lucas de Moraes Claro" and all anonymous contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("[+] We're on-line\n");
+    return 0;
+    
+}
