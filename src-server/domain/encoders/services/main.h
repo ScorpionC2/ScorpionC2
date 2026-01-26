a6181dd1357fb9fd435146d8dc2110df40905e17
Yyax13 <Yyax13@proton.me>
2026-01-26 15:51:14 -0300

    feat(domain/encoders): Created main.h with base docs to define the project default architecture for encoders

---
+++ src-server/domain/encoders/services/main.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2026-Present ScorpionC2 public-person "Lucas de Moraes Claro" and all anonymous contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
+
+#pragma once
+
+#include "../types/main.h"
+
+/*
+ * This file must contain all encoders as pointers to their implementations.
+ * Example:
+ *      #include "xor/main.h" // this main.h declares extern XorInstance.
+ *      
+ *      extern EncoderInstance Xor = XorInstance
+ * 
+ * The idea is to have all encoders in just one file, without the need to include every implementation.
+ */
+ 
