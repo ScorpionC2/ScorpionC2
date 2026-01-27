88faa02fe6a620b2875f28cda6cd522847dfcdcb
Yyax13 <Yyax13@proton.me>
2026-01-27 16:30:51 -0300

    ref(cli/loading): Deleted useless cli/loading modules

--- src-server/app/cli/loading/main.h
+++
@@ -1,6 +0,0 @@
-//
-// Copyright (c) 2026-Present ScorpionC2 public-person "Lucas de Moraes Claro" and all anonymous contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for details.
-//
-
-#pragma once
