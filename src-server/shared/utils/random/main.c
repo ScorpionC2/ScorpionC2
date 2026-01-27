734abb31ea39def2c4033babc473e7fc6109f0d1
Yyax13 <Yyax13@proton.me>
2026-01-27 17:43:42 -0300

    fix(utils/random): Fixed modulo bias in randr using limit and rejecting trash values; Refactored seed_g incrementation;

--- src-server/shared/utils/random/main.c
+++ src-server/shared/utils/random/main.c
@@ -9,67 +9,20 @@
 //  https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf
 // This file contains upgraded versions of that formula
 // 
+// Last updates and fixes was based in:
+//  https://romailler.ch/2020/07/28/crypto-modulo_bias_guide/?utm_source=reddit.com
+//  https://stackoverflow.com/questions/10984974/why-do-people-say-there-is-modulo-bias-when-using-a-random-number-generator?utm_source=google.com
+//  https://www.pcg-random.org/posts/xoroshiro-fails-truncated.html?utm_source=reddit.com
+//  https://en.wikipedia.org/wiki/Random_number_generation
+//  https://en.wikipedia.org/wiki/Randomness_extractor?utm_source=google.com
+// 
+// ps: removed the large "docs" because it's useless
+// 
 
 #include "main.h"
 #include <unistd.h>
 #include <stdint.h>
 #include <time.h>
-
-/*
- * Explaining this file:
- * 
- * seed (a.k.a. Randomseed) set the seed_g with the logic:
- *      seed_g = seed ^ (pid * (seed ^ (time >> 5)) * ((time * 571205793) >> 9 ))
- * 
- * rseed (a.k.a. Randomrseed) is used to random the seed if user don't changed seed_g for 1.
- * The logic is:
- *      return ((((seed * (time ^ pid)) ^ (seed / pid) + (time - pid)) * 2677735761u) % (((((seed * seed) ^ seed) - 25) + 569821785 (seed % 8))
- * 
- * rand (a.k.a. Randomrand) is used to get pseudo-random values with the logic:
- *      if (seed_g == 1) {
- *          s = rseed(seed_g ^ (3290751 * seed_g) + (rseed(seed_g * 49091339) >> 15))
- *      
- *      } else {
- *          s = seed_g
- * 
- *      }
- * 
- *      s = ((s << 13) ^ s) - (s >> 21)
- * 
- *      n = s
- *      n = (n * (n * n * 15732 + 789221) + 771171059)
- *      n += s
- *      n = ((n << 14) ^ n) - (n >> 20)
- *      
- *      x = n
- *      x = (x * (x * 15731) + 2821160599)
- *      x += n
- *      x = ((x << 19) ^ x) - (x >> 8)
- *      x += 0x9E3779B9
- * 
- *      y = x
- *      y = (y * (y * y * 15731 + 789221) + 771171059)
- *      y ^= y >> 16
- * 
- *      z = y
- *      z = (z * 2650005761u) ^ z
- * 
- *      a = z
- *      a = (a * (a * 0x9E3679B8u + a + 4322699887))
- * 
- *      b = a
- *      b = b * b * 0x9E3779B9u + b + 0x7F4A7C15u
- *      b ^= b >> 28
- * 
- *      out = b
- *      seed_g = out + 1
- * 
- *      return out
- * 
- * randr (a.k.a. Randomrandr) is used to get pseudo-random values in a range using this logic:
- *      return (rand() % (max - min + 1)) + min;
- * 
- */
 
 uint32_t seed_g = 1;
 
@@ -86,6 +39,7 @@ uint32_t Randomrseed(int seed) {
 uint32_t Randomrand() {
     uint32_t s = seed_g;
     if (seed_g == 1) {
+        seed_g ^= (seed_g * 257219671) + 0x9E3779B9;
         s = Randomrseed(seed_g ^ (3290751 * seed_g) + (Randomrseed(seed_g * 49091339) >> 15));
     
     }
@@ -117,15 +71,23 @@ uint32_t Randomrand() {
     b = b * b * 0x9E3779B9u + b + 0x7F4A7C15u;
     b ^= b >> 28;
     
-    uint32_t out = b;
-    seed_g = out + 1;
+    uint32_t out = b ^ (b >> 8) ^ (b >> 16) ^ (b >> 24);
+    seed_g ^= out + 0x9E3779B9;
     
     return out;
     
 };
 
 uint32_t Randomrandr(int min, int max) {
-    return (Randomrand() % (max - min + 1)) + min;
+    uint32_t range = (uint32_t)(max - min + 1);
+    uint32_t x;
+    uint32_t limit = UINT32_MAX - (UINT32_MAX % range);    
+    do {
+        x = Randomrand();
+    
+    } while (x >= limit);
+    
+    return (x % range) + min;
     
 }
 
