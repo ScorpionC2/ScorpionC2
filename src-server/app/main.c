db5135e8472b35c86b153c7bdd32997310bf49b4
Yyax13 <Yyax13@proton.me>
2026-01-27 17:44:52 -0300

    feat(app/main): Added tests for the new hashing algorithms

--- src-server/app/main.c
+++ src-server/app/main.c
@@ -53,7 +53,29 @@ int main() {
     free(unxored16.b);
 
     bytes_t djb2 = Hash.djb2(nameBytes);
-    Logger.bytesf(FG_WHITE_ITALIC "Hash for string \"howo\" (djb2): " RESET BG_PINK FG_WHITE_ITALIC "%bh" RESET "\n", &djb2);
+    Logger.bytesf(FG_WHITE_ITALIC "\n\nHash for string \"howo\" (djb2): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &djb2);
+    
+    bytes_t scorpion128x = Hash.scorpion128x(nameBytes);
+    Logger.bytesf(FG_WHITE_ITALIC "\nHash for string \"howo\" (scorpion128x): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &scorpion128x);
+    
+    bytes_t scorpion256x = Hash.scorpion256x(nameBytes);
+    Logger.bytesf(FG_WHITE_ITALIC "\nHash for string \"howo\" (scorpion256x): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &scorpion256x);
+    
+    bytes_t scorpion512x = Hash.scorpion512x(nameBytes);
+    Logger.bytesf(FG_WHITE_ITALIC "\nHash for string \"howo\" (scorpion512x): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &scorpion512x);
+    
+    bytes_t scorpion1024x = Hash.scorpion1024x(nameBytes);
+    Logger.bytesf(FG_WHITE_ITALIC "\nHash for string \"howo\" (scorpion1024x): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &scorpion1024x);
+    
+    bytes_t scorpion2048x = Hash.scorpion2048x(nameBytes);
+    Logger.bytesf(FG_WHITE_ITALIC "\nHash for string \"howo\" (scorpion2048x): " RESET BG_PURPLE FG_WHITE_ITALIC "%bh" RESET "\n", &scorpion2048x);
+    
+    free(djb2.b);
+    free(scorpion128x.b);
+    free(scorpion256x.b);
+    free(scorpion512x.b);
+    free(scorpion1024x.b);
+    free(scorpion2048x.b);
     
     return 0;
     
