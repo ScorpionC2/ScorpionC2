27fb37190a9051e9f2840a0422e003d687968be6
Felipe Paraizo <fparaizo3@gmail.com>
2026-03-03 15:53:34 -0300

    feat: add CLI input loop

--- src-server/app/main.c
+++ src-server/app/main.c
@@ -7,6 +7,11 @@
 #include "../shared/utils/random/main.h"
 
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include "commands/main.h"
+#include "cli/logs/main.h"
+#include "cli/input/main.h"
 
 void init() {
     Random.seed(time(NULL));
@@ -15,7 +20,31 @@ void init() {
 }
 
 int main() {
-    return 0;
-    
-}
+    init();
 
+    InputSettings conf;
+    conf.prompt.s = "scorpion> ";
+    conf.prompt.len = strlen(conf.prompt.s);
+    conf.histPath.s = "./.scorpion_history";
+    conf.histPath.len = strlen(conf.histPath.s);
+    conf.histLimit = 1000;
+
+    while (1) {
+        string_t input;
+        Input.readline(conf, &input);
+        if (input.s == NULL) {
+            continue;
+        }
+
+        int res = Commands.execute(input);
+        free(input.s);
+        if (res == 1) {
+            Logger.infoln("Exiting...");
+            break;
+        } else if (res == -1) {
+            Logger.warnln("Command execution error");
+            continue;
+        }
+    }
+    return 0;
+}
